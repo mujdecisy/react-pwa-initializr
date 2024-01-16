e0e466b3b6e13fe50d4ea2778baf8b64555ca9bf
mujdecisy <mujdecisy@gmail.com>
2024-01-17 00:03:36 +0300

    refactor: decompose route prefix to constants

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -4,12 +4,13 @@ import Main from './pages/main';
 import About from './pages/about';
 import MuLayout from './components/mulayout';
 import MuImportExport from './components/muimportexport';
+import { ROUTE_PREFIX } from './utils/constants';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/pwa-<<projectNameKebab>>/" element={<MuLayout />}>
+        <Route path={`${ROUTE_PREFIX}/`} element={<MuLayout />}>
           <Route index element={<Main />} />
           <Route path="importexport" element={<MuImportExport />} />
           <Route path="about" element={<About />} />
--- template/src/utils/constants.ts
+++ template/src/utils/constants.ts
@@ -1,4 +1,5 @@
 const APP_NAME = "<<projectNamePascal>>"
+const ROUTE_PREFIX = "/pwa-<<projectNameKebab>>"
 
 
-export {APP_NAME};
+export {APP_NAME, ROUTE_PREFIX};
